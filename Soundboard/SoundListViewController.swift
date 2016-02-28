1077db201c28a8fd4f74b0e4150da2fd9b75d659
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2016-02-28 13:44:17 -0600

    modify table view
    import test mp3 file; import avfoundation lib; add response in table view

--- Soundboard/SoundListViewController.swift
+++ Soundboard/SoundListViewController.swift
@@ -7,12 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
 
-class SoundListViewController: UIViewController {
+class SoundListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    var audioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        cell.textLabel!.text = "LALLAAL"
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let soundPath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3")
+        let soundURL = NSURL.fileURLWithPath(soundPath!)
+        
+        try! self.audioPlayer = AVAudioPlayer(contentsOfURL: soundURL)
+        self.audioPlayer.play()
     }
 }
 
